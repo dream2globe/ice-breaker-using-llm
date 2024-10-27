1cdf8f5ffa8c948ee5d5c298db1b9cb5019d2e7a
Kang Seonghyeon <shyeon.kang@gmail.com>
2024-10-27 23:14:23 +0900

    section2-linkedin-scraping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -7,24 +6,25 @@ from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
     load_dotenv()
+
     print("Hello LangChain")
 
     summary_template = """
-    given the information {information} about a person I want you to create:
+    given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
-    
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
-        mock=True
-    )
-        
-    prompt = PromptTemplate.from_template(summary_template)
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=2048)
-    output_parser = StrOutputParser()
 
-    chain = prompt | llm | output_parser
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+
+    chain = summary_prompt_template | llm
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+    )
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
